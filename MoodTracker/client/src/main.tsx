48b3b10ddc069fa0c9c7bd2267b0575e6532d32f
zeebie-new <shamimrafaqat613@gmail.com>
2025-06-14 01:18:27 +0500

    Update main.tsx

--- MoodTracker/client/src/main.tsx
+++ MoodTracker/client/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
